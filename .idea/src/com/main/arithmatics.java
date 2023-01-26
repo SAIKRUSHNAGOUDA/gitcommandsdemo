071a466662ccc6b0eb2a7e4dd8173474687e4590
sai gouda <saikrushnagouda@gmail.com>
2023-01-26 22:28:39 +0530

    its my 2nd commit

---
+++ .idea/src/com/main/arithmatics.java
@@ -0,0 +1,28 @@
+package com.main;
+
+public class arithmatics {
+    public int add(int a ,int b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        arithmatics arithmetic = new arithmatics();
+        System.out.println("Adding two number - " +arithmetic.add(2,1));
+        System.out.println("substitute two number - " +arithmetic.sub(2,1));
+        System.out.println("multiplying two number - " +arithmetic.multi(2,1));
+        System.out.println("divided two number - " +arithmetic.div(2,1));
+
+    }
+    public int sub(int a ,int b){
+        return a-b;
+    }
+    public int multi(int a ,int b){
+        return a*b;
+    }
+    public int div(int a ,int b){
+        return a/b;
+    }
+
+
+}
+
---
+++ .idea/src/com/main/main.java
@@ -0,0 +1,29 @@
+package com.main;
+
+import java.io.*;
+
+    public class main {
+        public static void main(String[] args)
+        {
+
+            float num1 = 10, num2 = 20, sum , sum1 , sum2  , sum4;
+            float sum3;
+
+
+            System.out.println("num1 = " + num1);
+            System.out.println("num2 = " + num2);
+
+
+            sum = num1 + num2;
+            sum1 = num1 - num2;
+            sum2 = num1 * num2;
+            sum3 = num1 / num2;
+            sum4 = num1 % num2;
+            System.out.println("The sum = " + sum);
+            System.out.println("The sub = " + sum1);
+            System.out.println("The mul = " + sum2);
+            System.out.println("The div = " + sum3);
+            System.out.println("The mud = " + sum4);
+        }
+    }
+
