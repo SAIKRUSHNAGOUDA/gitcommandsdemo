95a8cf89e8354be159381bb3155f749faf1bb910
sai gouda <saikrushnagouda@gmail.com>
2023-01-27 10:18:03 +0530

    its my 2nd commite3ui

--- .idea/src/com/main/arithmatics.java
+++ .idea/src/com/main/arithmatics.java
@@ -11,7 +11,7 @@ public class arithmatics {
         System.out.println("substitute two number - " +arithmetic.sub(2,1));
         System.out.println("multiplying two number - " +arithmetic.multi(2,1));
         System.out.println("divided two number - " +arithmetic.div(2,1));
-
+        System.out.println("Adding two number - " +arithmetic.add(2,1));
     }
     public int sub(int a ,int b){
         return a-b;
